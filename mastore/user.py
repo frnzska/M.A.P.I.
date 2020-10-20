85177c856ef76b4a679165ed1ff37504cb70a49e
frnzska <minziska@googlemail.com>
2020-10-20 20:34:11 +0200

    find method

--- mastore/user.py
+++ mastore/user.py
@@ -1,3 +1,6 @@
+import functools
+import sqlite3
+
 class User:
 
 
@@ -5,3 +8,21 @@ class User:
         self.id = id
         self.username = username
         self.password = password
+
+
+    def find_by_name(cls, name):
+        user = None
+        con = sqlite3.connect('data.db')
+        cursor = con.cursor()
+        query = 'SELECT * FROM users WHERE name=?'
+        results = cursor.execute(query, (name,))
+        row = results.fetchone()
+        con.close()
+        if row:
+             user = cls(*row)
+        return user
+
+
+
+
+
