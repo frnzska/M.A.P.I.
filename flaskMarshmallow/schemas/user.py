09fc2261a9f626067fdb81ab34f0463791b1b3bc
frnzska <minziska@googlemail.com>
2021-01-11 13:49:38 +0100

    formatting

--- flaskMarshmallow/schemas/user.py
+++ flaskMarshmallow/schemas/user.py
@@ -1,7 +1,11 @@
-from marshmallow import Schema, fields
+from models.user import UserModel
+from ma import ma
 
-class UserSchema(Schema):
-
-    class Meta:
-        load_only = ('password',) # no returning/dumping of password, just receiving. Note its tuple
-        dump_only = ('user_id',)
+class UserSchema(ma.SQLAlchemyAutoSchema): # app initialisation in app.py
+    class Meta: # keyword variables!
+        model = UserModel  # checks fields defined in Usermodel and creating them
+        load_only = (
+            "password",
+        )  # no returning/dumping of password, just receiving. Note its tuple
+        dump_only = ("user_id",)
+        load_instance = True # without this dict instaead of usermodel obj loaded
